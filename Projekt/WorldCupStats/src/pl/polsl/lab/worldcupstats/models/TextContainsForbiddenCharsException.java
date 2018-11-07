9c5a5007327e2277cfd28a26d40a6e7e7c6c13d9
millhavenx666 <millhavenx666@gmail.com>
2018-11-07 13:01:22 +0100

    v1.0

---
+++ Projekt/WorldCupStats/src/pl/polsl/lab/worldcupstats/models/TextContainsForbiddenCharsException.java
@@ -0,0 +1,29 @@
+package pl.polsl.lab.worldcupstats.models;
+
+/**
+ *
+ * Exception class for objects thrown when user try to set team name text
+ * contains of other charactres than letters (like numbers, commas...) or is
+ * empty
+ *
+ * @author Paweł Szwajnoch
+ * @version 1.0
+ */
+public class TextContainsForbiddenCharsException extends Exception {
+
+    public TextContainsForbiddenCharsException() {
+        super();
+    }
+
+    public TextContainsForbiddenCharsException(String message) {
+        super(message);
+    }
+
+    public TextContainsForbiddenCharsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public TextContainsForbiddenCharsException(Throwable cause) {
+        super(cause);
+    }
+}
